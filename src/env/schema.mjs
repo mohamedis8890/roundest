6de99d6a2e83b74bd6f31ba31dae93a199461d3d
mohamedis8890 <mohamed.is8890@gmail.com>
2022-11-15 03:06:29 +0200

    Disable Server schema untill we use env variables

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -6,6 +6,7 @@ import { z } from "zod";
  * This way you can ensure the app isn't built with invalid env vars.
  */
 export const serverSchema = z.object({
+  /*
   DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
   NEXTAUTH_SECRET:
@@ -21,6 +22,7 @@ export const serverSchema = z.object({
   ),
   DISCORD_CLIENT_ID: z.string(),
   DISCORD_CLIENT_SECRET: z.string(),
+*/
 });
 
 /**
