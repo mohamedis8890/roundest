b58647690c4d02cf135cda2f742878407848b6c7
mohamedis8890 <mohamed.is8890@gmail.com>
2022-11-17 03:50:34 +0200

    disable checking server env schema

--- src/env/server.mjs
+++ src/env/server.mjs
@@ -7,7 +7,7 @@ import { serverSchema } from "./schema.mjs";
 import { env as clientEnv, formatErrors } from "./client.mjs";
 
 const _serverEnv = serverSchema.safeParse(process.env);
-
+/*
 if (!_serverEnv.success) {
   console.error(
     "❌ Invalid environment variables:\n",
@@ -15,7 +15,7 @@ if (!_serverEnv.success) {
   );
   throw new Error("Invalid environment variables");
 }
-
+*/
 for (let key of Object.keys(_serverEnv.data)) {
   if (key.startsWith("NEXT_PUBLIC_")) {
     console.warn("❌ You are exposing a server-side env-variable:", key);
